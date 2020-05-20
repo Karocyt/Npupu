5b3740c309f7309e08868f8b57cfc4a5c7faf3a0
Blindiak <blindiak@gmail.com>
2020-05-20 18:58:21 +0200

    heuristic isok?

--- internal/heuristics/toop.go
+++ internal/heuristics/toop.go
@@ -1,14 +1,11 @@
 package heuristics
 
-func tScore(grid []int, nb int, x int, y int, size int) float32 {
-	tmp := nb_pos[nb]
-	x1 := tmp[0]
-	y1 := tmp[1]
-	nb1 := grid[get1d(x1, y1, size)]
+func tScore(puzzle []int, nb int, x int, y int, size int) float32 {
+	nb1 := puzzle[get1d(x, y, size)]
 	if nb == nb1 {
-		return 1
-	} else {
 		return 0
+	} else {
+		return 1
 	}
 	return 0
 }
@@ -16,10 +13,10 @@ func tScore(grid []int, nb int, x int, y int, size int) float32 {
 // Tiles out-of place
 func toop(grid []int, size int) float32 {
 	var score float32
-	_, nb_pos = makeGoal(size)
+	puzzle, _ := makeGoal(size)
 	for x := 0; x < size; x++ {
 		for y := 0; y < size; y++ {
-			score += tScore(grid, grid[get1d(x, y, size)], x, y, size)
+			score += tScore(puzzle, grid[get1d(x, y, size)], x, y, size)
 		}
 	}
 	return score
