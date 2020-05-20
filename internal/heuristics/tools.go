91dd55326bee9dd1d365248c78df3f64fc78b6db
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2020-05-20 17:06:02 +0200

    corrected makeGoal

--- internal/heuristics/tools.go
+++ internal/heuristics/tools.go
@@ -1,52 +1,47 @@
 package heuristics
 
 func abs(nb int) int {
-	if 	nb < 0 {
+	if nb < 0 {
 		return -nb
 	} else {
 		return nb
 	}
 }
 
-func get_1d(x int, y int, size int) int {
-	return x * size + y
+func get1d(x int, y int, size int) int {
+	return x*size + y
 }
 
-func get_2d(nb int, size int) (int, int) {
+func get2d(nb int, size int) (int, int) {
 	var x, y int
 	x = nb / size
 	y = nb % size
 	return x, y
 }
 
-func make_goal(s int ) []int {
-	ts := s * s
-	puzzle := make([]int, ts)
+func makeGoal(s int) []int {
+	puzzle := make([]int, s*s)
 	cur := 1
 	x := 0
 	ix := 1
 	y := 0
 	iy := 0
-	for {
+	for cur < s*s {
 		puzzle[x+y*s] = cur
 
-		if cur == 0 {
-			break
-		}
-		cur += 1
+		cur++
 
-		if x+ix == s || x+ix < 0 || (ix != 0 && puzzle[x+ix+y*s] != -1) {
+		if x+ix == s || x+ix < 0 || (ix != 0 && puzzle[x+ix+y*s] != 0) {
 			iy = ix
 			ix = 0
-		} else if y+iy == s || y+iy < 0 || (iy != 0 && puzzle[x+(y+iy)*s] != -1) {
+		} else if y+iy == s || y+iy < 0 || (iy != 0 && puzzle[x+(y+iy)*s] != 0) {
 			ix = -iy
 			iy = 0
-			x += ix
-			y += iy
 		}
-		if cur == s*s {
-			cur = 0
-		}
+		x += ix
+		y += iy
 	}
+	puzzle[x+y*s] = 0
+
 	return puzzle
 }
--- internal/heuristics/toop.go
+++ internal/heuristics/toop.go
@@ -1,8 +1,8 @@
 package heuristics
 
-func t_score(grid []int, nb int, x int, y int, size int) float32 {
-	x1, y1 := get_2d(nb, size)
-	nb1 := grid[get_1d(x1 , y1, size)]
+func tScore(grid []int, nb int, x int, y int, size int) float32 {
+	x1, y1 := get2d(nb, size)
+	nb1 := grid[get1d(x1, y1, size)]
 	if nb == nb1 {
 		return 1
 	} else {
@@ -16,7 +16,7 @@ func toop(grid []int, size int) float32 {
 	var score float32
 	for x := 0; x < size; x++ {
 		for y := 0; y < size; y++ {
-			score += t_score(grid, grid[get_1d(x , y, size)], x, y, size)
+			score += tScore(grid, grid[get1d(x, y, size)], x, y, size)
 		}
 	}
 	return score
