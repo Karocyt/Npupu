5b9ed7c20fde630add0427a4d0a14b1523b9d4fc
Karocyt/kevazoul <azoulay.kevin+robot@gmail.com>
2020-05-23 20:40:49 +0200

    solves solvable

--- internal/solver/Solver.global.go
+++ internal/solver/Solver.global.go
@@ -13,7 +13,7 @@ import (
 ** return value: error e (unsolvable)
  */
 
-func bestScore (l []*gridState) (cur *gridState) {
+func bestScore(l []*gridState) (cur *gridState) {
 	for _, item := range l {
 		if cur == nil || item.score < cur.score {
 			cur = item
@@ -22,38 +22,52 @@ func bestScore (l []*gridState) (cur *gridState) {
 	return cur
 }
 
-func (solver *Solver) findState (state *gridState) int {
-	for i, find := range  solver.openedStates {
+func (solver *Solver) findState(state *gridState) int {
+	for i, find := range solver.openedStates {
 		if find == state {
+			fmt.Printf("find: %p, state: %p\n", find, state)
 			return i
 		}
 	}
 	return -1
 }
 
-func (solver *Solver) closeState (state *gridState) {
+func (solver *Solver) closeState(state *gridState) {
 	idx := solver.findState(state)
 	if idx == -1 {
 		fmt.Println("NANI?! idx -1 ? ")
 	}
-	solver.openedStates[idx] = solver.openedStates[len(solver.openedStates)-1] 
+	solver.openedStates[idx] = solver.openedStates[len(solver.openedStates)-1]
+	solver.openedStates[len(solver.openedStates)-1] = nil // To remove ?
 	solver.openedStates = solver.openedStates[:len(solver.openedStates)-1]
 }
 
+// Solve solve
 func (solver *Solver) Solve() (e error) {
 	cur := solver.openedStates[0]
 	for cur != nil && cur.score != 0 {
 		nextStates := cur.generateNextStates()
 		solver.explored[cur.mapKey()] = true
+		//fmt.Println(cur)
 
-		for _, child := range  nextStates {
-			if solver.explored[child.mapKey()] == false {
-				child.score = solver.fn(child.grid, size, child.depth)
-				solver.openedStates = append(solver.openedStates, &child)
+		for i := range nextStates {
+			if solver.explored[nextStates[i].mapKey()] == false {
+				nextStates[i].score = solver.fn(nextStates[i].grid, size, nextStates[i].depth)
+				solver.openedStates = append(solver.openedStates, &nextStates[i])
 			}
 		}
+		fmt.Println("\n\n\nBefore:", solver.openedStates)
 		solver.closeState(cur)
+		fmt.Println("After:", solver.openedStates)
+
+		//fmt.Println("opened:", solver.openedStates)
 		cur = bestScore(solver.openedStates)
+		var tmp *gridState
+		for tmp != cur && cur.score == 0 {
+			cur.score = solver.fn(cur.grid, size, cur.depth)
+			tmp = cur
+			cur = bestScore(solver.openedStates)
+		}
 	}
 	if len(solver.openedStates) == 0 {
 		return errors.New("Error: pupu not solvable(empty open states)")
--- internal/solver/Solver.go
+++ internal/solver/Solver.go
@@ -3,21 +3,19 @@ package solver
 // scoreFn type: heuristic functions prototype
 type scoreFn func([]int, int, int) float32
 
-
 var size int
 
 // Solver contains all variables required to solve the grid
 // Solver.Solution contains ordered states from the starting grid to the solved one
 type Solver struct {
-	openedStates    	[]*gridState
-	Solution        	[]*gridState
-	fn              	scoreFn
-	explored        	map[string]bool
-	maxOpenedStates		int
-	totalStates 		int
-	depth				int
+	openedStates    []*gridState
+	Solution        []*gridState
+	fn              scoreFn
+	explored        map[string]bool
+	maxOpenedStates int
+	totalStates     int
+	depth           int
 }
-
 
 // New initialize a new solverStruct, required to disciminate variables in multi-solving
 // Can be removed if we don't need to initialize anything
@@ -25,15 +23,15 @@ type Solver struct {
 func New(grid []int, gridSize int, fn scoreFn, greedy bool) Solver {
 	solver := Solver{
 		totalStates: 1,
-		fn: fn,
-		explored: make(map[string]bool, 100 * size * size),
+		fn:          fn,
+		explored:    make(map[string]bool, 100*size*size),
 	}
 
 	size = gridSize
 	state := gridState{
 		grid:  grid,
 		depth: 1,
-		score: fn(grid, gridSize, solver.depth),
+		score: fn(grid, gridSize, 1),
 	}
 
 	solver.openedStates = append(solver.openedStates, &state)
@@ -45,8 +43,6 @@ func (solver *Solver) hasSeen(state gridState) bool {
 	key := state.mapKey()
 	return solver.explored[key]
 }
-
-
 
 // PrintStats does exactly what it says
 func (solver *Solver) PrintStats() {
