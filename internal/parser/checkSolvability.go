73f4f8dc8f6bfb6e1dc59ae662f42f0c37260f73
Blindiak <blindiak@gmail.com>
2020-05-25 02:27:51 +0200

    check not solvable broken du cul dodo .... ZzZZZZz.....

--- internal/parser/checkSolvability.go
+++ internal/parser/checkSolvability.go
@@ -7,12 +7,12 @@ import (
 )
 
 func countInv(pupu []int, size int) (invCount int, posEmpty int) {
-	_, goal := heuristics.MakeGoal(size)
-
+	fuu, goal := heuristics.MakeGoal(size)
+fmt.Println(fuu, goal)
 	get1D := func (lol int) int {
 
 		x, y := goal[lol][0], goal[lol][1]
-		fmt.Println(heuristics.Get1d(x, y, size))
+	//	fmt.Println(heuristics.Get1d(x, y, size))
 		return  heuristics.Get1d(x, y, size)
 	}
 	posEmpty = size
@@ -21,19 +21,14 @@ func countInv(pupu []int, size int) (invCount int, posEmpty int) {
 	for i := 0; i < size * size - 1; i++ {
 		for j := (i + 1); j < size*size; j++ {
 			posN1 := get1D(pupu[i])
-			fmt.Println(posN1, i, pupu[i])
+	//		fmt.Println(posN1, i, pupu[i])
 			posN2 := get1D(pupu[j])
-			fmt.Println(posN2, i, pupu[j])
-			if pupu[j] != 0 && pupu[i] != 0 && posN1 > posN2 {
+	//		fmt.Println(posN2, i, pupu[j])
+			if  /* NANI */ posN1 > posN2 {
 				invCount++
 			}
 			if pupu[j] == 0 && j != size * size - 1 {
 				posEmpty = size - (j / size)
-				posN1 = get1D(pupu[i])
-				posN2 = get1D(pupu[j + 1])
-				if posN1 > posN2 {
-					invCount++
-				}
 			}
 		}
 	}
